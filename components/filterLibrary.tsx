bdf8e8174c4d3429a2c2f02aa867bfbace155ef6
Spriha Jha <jha.spriha@gmail.com>
2023-01-20 21:17:46 +0000

    Updated filterlibrary

--- components/filterLibrary.tsx
+++ components/filterLibrary.tsx
@@ -1,28 +1,86 @@
 import { motion } from "framer-motion";
 import Image from "next/image";
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Slider from "@mui/material/Slider";
 
+function valuetext(value: number) {
+  return `${value}°C`;
+}
+
+function createData(
+  name: number,
+  calories: number,
+  fat: number,
+  carbs: number,
+  protein: number
+) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData(0.1, 159, 6.0, 24, 4.0),
+  createData(0.1, 237, 9.0, 37, 4.3),
+  createData(0.1, 262, 16.0, 24, 6.0),
+  createData(0.1, 305, 3.7, 67, 4.3),
+  createData(0.1, 356, 16.0, 49, 3.9),
+];
 const FilterLibrary = () => {
+  const [value, setValue] = React.useState<number[]>([20, 37]);
+
+  const handleChange = (event: Event, newValue: number | number[]) => {
+    setValue(newValue as number[]);
+  };
   return (
-    <header className="sticky top-0 p-5 flex items-center justify-center z-20">
-      <motion.div
-        className="flex flex-row items-center"
-        initial={{
-          y: -500,
-          opacity: 0,
-          scale: 0.5,
-        }}
-        animate={{
-          y: 0,
-          opacity: 1,
-          scale: 1,
-        }}
-        transition={{
-          duration: 0.5,
-        }}
-      >
-        <div className="pt-5">HII</div>
-      </motion.div>
-    </header>
+    <>
+      <h6>Based on Lipinski&apos;s properties</h6>
+      <Box sx={{ width: 300 }}>
+        <Slider
+          getAriaLabel={() => "Temperature range"}
+          value={value}
+          onChange={handleChange}
+          valueLabelDisplay="auto"
+          getAriaValueText={valuetext}
+          color="secondary"
+        />
+      </Box>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>SMILES</TableCell>
+              <TableCell>Molecular Weight</TableCell>
+              <TableCell align="right">LogP</TableCell>
+              <TableCell align="right">TPSA</TableCell>
+              <TableCell align="right">Similarity Score</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow
+                key={row.name}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="right">{row.calories}</TableCell>
+                <TableCell align="right">{row.fat}</TableCell>
+                <TableCell align="right">{row.carbs}</TableCell>
+                <TableCell align="right">{row.protein}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
   );
 };
 
