d228cdc941e6e27a7d8275912554372fdfaab6d2
Spriha Jha <jha.spriha@gmail.com>
2023-01-20 21:04:48 +0000

    Added footer to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
 import ScrollPage from "pages/parallaxScroll";
 import Header from "@/components/header";
+import Footer from "@/components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function Home() {
       <main className={styles.main}>
         <Header />
         <ScrollPage />
+        <Footer />
       </main>
     </>
   );
